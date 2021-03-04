98cf7004bfb61048512388921b7e245a85385ce3
KHAN ADIL <36574088+khann-adill@users.noreply.github.com>
2021-03-04 10:44:45 +0530

    Update OrderDao.java

--- src/main/java/com/javatechie/aws/cicd/example/OrderDao.java
+++ src/main/java/com/javatechie/aws/cicd/example/OrderDao.java
@@ -14,6 +14,7 @@ public class OrderDao {
         return Stream.of(
                 new Order(101, "Mobile", 1, 30000),
                 new Order(58, "Book", 4, 2000),
+                new Order(1806, "Adil", 06, 1997),
                 new Order(205, "Laptop", 1, 150000),
                 new Order(809, "headset", 1, 1799))
                 .collect(Collectors.toList());
